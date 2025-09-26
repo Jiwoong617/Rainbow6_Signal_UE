41fc0698223a4aa057752f8cad9b732685f5a992
허지웅 <joe0617160@naver.com>
2025-09-26 15:57:39 +0900

    match schema, add endui

--- Source/Rainbow6_Signal/Public/SignalData.h
+++ Source/Rainbow6_Signal/Public/SignalData.h
@@ -19,20 +19,47 @@ namespace NetworkConfig
 	}
 }
 
-//언리얼측에서 서버로 보낼 정보
+//언리얼측에서 서버로 보낼 정보 - Start
 USTRUCT(BlueprintType)
-struct FSignalSendData
+struct FSignalStartData
 {
 	GENERATED_BODY()
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Type = TEXT("start_scenario"); // start_scenario
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FString Scenario; // 현재 시나리오(손동작) 이름
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 Fps; // fps 수
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TArray<FString> Frames; // Base64 이미지 정보
+	int32 Fps; // 총 fps 수
 };
+
+//언리얼측에서 서버로 보낼 정보 - Frame
+USTRUCT(BlueprintType)
+struct FSignalFrameData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Type = TEXT("frame"); // frame
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 FrameId; // 현재 프레임 번호
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Frame; // Base64 이미지 정보
+};
+
+//언리얼측에서 서버로 보낼 정보 - End
+USTRUCT(BlueprintType)
+struct FSignalEndData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Type = TEXT("end_scenario"); // end_scenario
+};
+
 
 //서버측에서 언리얼로 보낼 정보
 USTRUCT(BlueprintType)
