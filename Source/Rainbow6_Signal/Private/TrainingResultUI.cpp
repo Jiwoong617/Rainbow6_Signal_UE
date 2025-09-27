dd87d69c211f6e43cbf21469f3b992b8064b41e2
prayslaks <prayslaks@naver.com>
2025-09-28 01:41:46 +0900

    2025-09-28 : UI 개선 작업 완료

---
+++ Source/Rainbow6_Signal/Private/TrainingResultUI.cpp
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TrainingResultUI.h"
+#include "Components/TextBlock.h"
+#include "Kismet/GameplayStatics.h"
+
+void UTrainingResultUI::Hide()
+{
+	this->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UTrainingResultUI::Show()
+{
+	this->SetVisibility(ESlateVisibility::Visible);
+	UGameplayStatics::PlaySound2D(GetWorld(), MR);
+}
+
+void UTrainingResultUI::SetBombScore(const float Score)
+{
+	TotalScore += Score;
+	TEXT_BombScore->SetText(FText::FromString(FString::SanitizeFloat(Score)));
+	UpdateTotalScoreAndGrade();
+}
+
+void UTrainingResultUI::SetHeliScore(const float Score)
+{
+	TotalScore += Score;
+	TEXT_HeliScore->SetText(FText::FromString(FString::SanitizeFloat(Score)));
+	UpdateTotalScoreAndGrade();
+}
+
+void UTrainingResultUI::SetTankScore(const float Score)
+{
+	TotalScore += Score;
+	TEXT_TankScore->SetText(FText::FromString(FString::SanitizeFloat(Score)));
+	UpdateTotalScoreAndGrade();
+}
+
+void UTrainingResultUI::SetGasScore(const float Score)
+{
+	TotalScore += Score;
+	TEXT_GasScore->SetText(FText::FromString(FString::SanitizeFloat(Score)));
+	UpdateTotalScoreAndGrade();
+}
+
+void UTrainingResultUI::UpdateTotalScoreAndGrade()
+{
+	TEXT_TotalScore->SetText(FText::FromString(FString::SanitizeFloat(TotalScore)));
+	FString Grade;
+	FString Comment;
+	if (TotalScore >= 90)
+	{
+		Grade = TEXT("특급");
+		Comment = TEXT("참 잘했어요!");
+	}
+	else if (TotalScore >= 80)
+	{
+		Grade = "1급";
+		Comment = TEXT("최고를 목표로 나아갈 것.");
+	}
+	else if (TotalScore >= 70)
+	{
+		Grade = "2급";
+		Comment = TEXT("더욱 정진할 수 있을 것.");
+	}
+	else if (TotalScore >= 60)
+	{
+		Grade = "3급";
+		Comment = TEXT("포기하지 말 것.");
+	}
+	else
+	{
+		Grade = "실격";
+		Comment = TEXT("");
+	}
+	
+	TEXT_TotalGrade->SetText(FText::FromString(Grade));
+	TEXT_CommentFromCommander->SetText(FText::FromString(Comment));
+}
---
+++ Source/Rainbow6_Signal/Public/TrainingResultUI.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TrainingResultUI.generated.h"
+
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class RAINBOW6_SIGNAL_API UTrainingResultUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_BombScore;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_HeliScore;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_TankScore;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_GasScore;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_TotalScore;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_TotalGrade;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* TEXT_CommentFromCommander;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* MR;
+
+	UPROPERTY()
+	float TotalScore;
+
+	UFUNCTION()
+	void Hide();
+
+	UFUNCTION(BlueprintCallable)
+	void Show();
+
+	UFUNCTION(blueprintCallable)
+	void SetBombScore(float Score);
+
+	UFUNCTION(blueprintCallable)
+	void SetHeliScore(float Score);
+
+	UFUNCTION(blueprintCallable)
+	void SetTankScore(float Score);
+
+	UFUNCTION(blueprintCallable)
+	void SetGasScore(float Score);
+
+	UFUNCTION(blueprintCallable)
+	void UpdateTotalScoreAndGrade();
+};
