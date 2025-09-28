952970ec2ca9ba73b683c801f6953ad055da289d
prayslaks <prayslaks@naver.com>
2025-09-29 00:09:09 +0900

    2025-09-29 : 수정

--- Source/Rainbow6_Signal/Private/TrainingResultUI.cpp
+++ Source/Rainbow6_Signal/Private/TrainingResultUI.cpp
@@ -46,35 +46,35 @@ void UTrainingResultUI::SetGasScore(const float Score)
 void UTrainingResultUI::UpdateTotalScoreAndGrade()
 {
 	TEXT_TotalScore->SetText(FText::FromString(FString::SanitizeFloat(TotalScore)));
-	FString Grade;
+	FText Grade;
 	FString Comment;
 	
 	if (TotalScore >= 90)
 	{
-		Grade = TEXT("특급");
+		Grade = FText::FromString(TEXT("특급"));
 		Comment = TEXT("참 잘했어요!");
 	}
 	else if (TotalScore >= 80)
 	{
-		Grade = "1급";
+		Grade = FText::FromString(TEXT("1급"));
 		Comment = TEXT("최고를 목표로 나아갈 것.");
 	}
 	else if (TotalScore >= 70)
 	{
-		Grade = "2급";
+		Grade = FText::FromString(TEXT("2급"));
 		Comment = TEXT("더욱 정진할 수 있을 것.");
 	}
 	else if (TotalScore >= 60)
 	{
-		Grade = "3급";
+		Grade = FText::FromString(TEXT("3급"));
 		Comment = TEXT("포기하지 말 것.");
 	}
 	else
 	{
-		Grade = "실격";
+		Grade = FText::FromString(TEXT("실격"));
 		Comment = TEXT("");
 	}
 	
-	TEXT_TotalGrade->SetText(FText::FromString(Grade));
+	TEXT_TotalGrade->SetText(Grade);
 	TEXT_CommentFromCommander->SetText(FText::FromString(Comment));
 }
